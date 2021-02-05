8a4ae7cc2acf50410a2b225e583dfc1d5959ea7b
pl4ko <konrad-plasser@gmx.at>
2021-02-05 16:08:23 +0100

    updated app

--- SteamApp/SettingsViewController.swift
+++ SteamApp/SettingsViewController.swift
@@ -12,7 +12,7 @@ class SettingsViewController: UIViewController{
     
     @IBOutlet weak var segment: UISegmentedControl!
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad() //hallo
         
         //test
      
