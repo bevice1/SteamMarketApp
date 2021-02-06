343a8f6bd1548be8a66a69f0cbcb39f3698854c1
bevice1 <bk@kaiser-web.at>
2021-02-06 17:26:47 +0100

    current currency is highlighted

--- SteamApp/SettingsViewController.swift
+++ SteamApp/SettingsViewController.swift
@@ -15,20 +15,30 @@ class SettingsViewController: UIViewController{
     @IBOutlet weak var segment: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad() //hallo Welt
+ 
+        var currentStoredIndex = 0
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
         
-//        segment.addTarget(self, action: #selector(segmentedControlValueChanged(segment: segment)), for:.touchUpInside)
-//        switch segment.selectedSegmentIndex{
-//        case 0:             print("Currency is $")
-//
-//        case 1:
-//            print("Currency is €")
-//
-//        case 2:
-//            print("Currency is Pfund")
-//
-//        default:
-//            print("error")
-//        }
+        let request = NSFetchRequest<Currency>(entityName: "Currency")
+        if let context = appDelegate?.persistentContainer.viewContext{
+            if let readEntry = try? context.fetch(request){
+                
+                for elem in readEntry{
+                    switch elem.currencyString {
+                    case "€":
+                        currentStoredIndex = 0
+                    case "$":
+                        currentStoredIndex = 1
+                    case "£":
+                        currentStoredIndex = 2
+                    default:
+                        currentStoredIndex = 0
+                    }
+                }
+                
+            }
+        }
+        segment.selectedSegmentIndex = currentStoredIndex
         
         segment.addTarget(self, action: #selector(SettingsViewController.indexChanged(_:)), for: .valueChanged)
         
@@ -36,32 +46,40 @@ class SettingsViewController: UIViewController{
     }
     @objc func indexChanged(_ sender: UISegmentedControl) {
         let appDelegate = UIApplication.shared.delegate as? AppDelegate
-
+        
         let request = NSFetchRequest<Currency>(entityName: "Currency")
         if let context = appDelegate?.persistentContainer.viewContext{
             if let readEntry = try? context.fetch(request){
+                
                 for elem in readEntry{
                     context.delete(elem)
                 }
-            
-            let currency = Currency(context: context)
-            if segment.selectedSegmentIndex == 0 {
-                print("Select €")
-                currency.currencyString = "€"
-            } else if segment.selectedSegmentIndex == 1 {
-                print("Select $")
-                currency.currencyString = "$"
-            } else if segment.selectedSegmentIndex == 2 {
-                print("Select Pfund")
-                currency.currencyString = "£"
-            }
-            
-            
-            appDelegate?.saveContext()
+                
+                let currency = Currency(context: context)
+                switch segment.selectedSegmentIndex{
+                case 0:
+                    print("Currency is $")
+                    currency.currencyString = "€"
+                    
+                case 1:
+                    print("Currency is €")
+                    currency.currencyString = "$"
+                    
+                case 2:
+                    print("Currency is Pfund")
+                    currency.currencyString = "£"
+                    
+                    
+                    
+                default:
+                    print("error")
+                }
+                
+                appDelegate?.saveContext()
             }
         }
-       
+        
     }
-
-
+    
+    
 }
